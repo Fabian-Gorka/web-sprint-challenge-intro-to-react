78993e2c093edc187d748242bd47d381ffe81a45
Fabian-Gorka <fabian-gorka@lambdastudents.com>
2021-02-19 15:38:16 -0600

    my test passes and im pretty sure i did the mvp right

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
+import Character from './components/Character.js';
+import styled from 'styled-components'
+import './Fonts.css'
+
+const Title = styled.h1`
+  -webkit-text-stroke: .5px yellow;
+  font-family: 'Poller One', cursive;
+  font-family: 'Righteous', cursive;
+  font-size: 75px;
+
+  `
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+ const [data, setData] = useState([])
+ useEffect(() => {
+   axios.get('https://swapi.dev/api/people')
+   .then((res) => {
+     setData(res.data)
+   })
+   .catch()
+ },[])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <Title className="Header">Characters</Title>
+      {data.map(element => Character({'data':element}))}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,89 @@
-// Write your Character component here
+import React from 'react';
+import styled from 'styled-components'
+import './CompFonts.css'
+
+const StyledDiv = styled.div`
+    color:darkgreen;
+    display:block;
+    text-align: left;
+    width: 55%;
+    margin-left:20%;
+    display:flex;
+    flex-wrap:wrap;
+    flex-direction: column;
+    justify-content:space-between;
+    align-items: center;
+    border:3px solid darkgreen;
+    padding:0px 20px;
+    background-color:black;
+    font-family: 'Righteous', cursive;
+    font-family: 'Poller One', cursive;
+    font-family: 'DotGothic16', sans-serif;
+    
+`
+const StyledH1 = styled.h1`
+    display:inline;
+`
+const StyledBD = styled.h2`
+    display:inline;
+    color:darkgreen;
+    font-family: 'Righteous', cursive;
+    font-family: 'Poller One', cursive;
+    font-family: 'DotGothic16', sans-serif;
+`
+const StyEyeColor = styled.p`
+    font-family: 'Righteous', cursive;
+    font-family: 'Poller One', cursive;
+    font-family: 'DotGothic16', sans-serif;
+    color:darkgreen;
+    margin-top: -6px;
+    `
+const StyHairColor = styled.p`
+font-family: 'Righteous', cursive;
+    font-family: 'Poller One', cursive;
+    font-family: 'DotGothic16', sans-serif;
+    color:darkgreen;
+    margin-top: -6px;
+    `
+
+const StyHeight = styled.p`
+font-family: 'Righteous', cursive;
+    font-family: 'Poller One', cursive;
+    font-family: 'DotGothic16', sans-serif;
+    color:darkgreen;
+    margin-top: -6px;
+    `
+
+const StyMass = styled.p`
+font-family: 'Righteous', cursive;
+    font-family: 'Poller One', cursive;
+    font-family: 'DotGothic16', sans-serif;
+    color:darkgreen;
+    margin-top: -6px;
+    `
+const StyGender = styled.p`
+font-family: 'Righteous', cursive;
+    font-family: 'Poller One', cursive;
+    font-family: 'DotGothic16', sans-serif;
+    color:darkgreen;
+    
+`
+const Character = props => {
+    const { data } = props;
+    console.log(data)
+
+ 
+
+  return (
+    <StyledDiv>
+        <StyledH1>{data.name}</StyledH1>
+        <StyledBD>DOB: {data.birth_year}</StyledBD> 
+        <StyGender>GENDER: {data.gender}</StyGender>
+        <StyEyeColor>EYE_COLOR: {data.eye_color}</StyEyeColor>
+        <StyHairColor>HAIR_COLOR: {data.hair_color}</StyHairColor>
+        <StyHeight>HEIGHT: {data.height}</StyHeight>
+        <StyMass>MASS: {data.mass}</StyMass>
+    </StyledDiv>
+  );
+}
+export default Character; 
